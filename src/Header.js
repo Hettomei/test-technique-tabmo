8dcf8174b71890420160943427b91090d49bc5a3
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2019-11-27 01:17:57 +0100

    Display number of pokemon in cart

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+
+import { Row, Col, Badge } from "reactstrap";
+
+const sum = (acc, n) => acc + n;
+
+const selectTotalInCart = createSelector(
+  state => state.cart,
+  cart => Object.values(cart).reduce(sum, 0)
+);
 
 export function Header() {
-  return <h1>Header</h1>;
+  const totalInCart = useSelector(selectTotalInCart);
+
+  return (
+    <Row>
+      <Col xs="10">
+        <h1>Tabmo - Pokeshop</h1>
+      </Col>
+      <Col xs="2">
+        <Badge color="primary" pill>
+          in cart: {totalInCart}
+        </Badge>
+      </Col>
+    </Row>
+  );
 }
