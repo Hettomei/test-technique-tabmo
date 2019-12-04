bfd12876cb9ac8ace9e6db96a6bd9df9d32f0e22
Timothée Gauthier <2079042+Hettomei@users.noreply.github.com>
2019-12-04 14:03:25 +0100

    Fix test.
    It didn't worked before because we didn't provide the reducers

--- src/AddToCart.test.js
+++ src/AddToCart.test.js
@@ -11,11 +11,19 @@ describe("AddToCart", () => {
   beforeEach(() => {
     store = mockStore({
       pokeapi: [
-        { name: "poke1", url: "https://pokeapi.co/api/v2/pokemon/1/", price: 10 },
-        { name: "poke2", url: "https://pokeapi.co/api/v2/pokemon/2/", price: 100 },
+        {
+          name: "poke1",
+          url: "https://pokeapi.co/api/v2/pokemon/1/",
+          price: 10
+        },
+        {
+          name: "poke2",
+          url: "https://pokeapi.co/api/v2/pokemon/2/",
+          price: 100
+        }
       ],
       cart: {
-        poke1: [1, 20, 300],
+        poke1: [1, 20, 300]
       }
     });
   });
@@ -63,13 +71,27 @@ describe("AddToCart", () => {
       .find("button")
       .last()
       .simulate("click");
-    console.log(store.getState());
-    wrapper.update();
 
-    expect(wrapper.text()).toEqual("-1+");
+    expect(store.getActions()).toEqual([
+      { pokemon: { name: "poke2", price: 100 }, type: "add-pokemon" }
+    ]);
   });
 
-  it("when click on - doesnt go below 0", () => {
-    throw new Error('to implement')
+  it("when click on - , decrement counter", () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <AddToCart pokemon={{ name: "poke2", price: 100 }} />
+      </Provider>
+    );
+
+    expect(wrapper.text()).toEqual("-0+");
+    wrapper
+      .find("button")
+      .first()
+      .simulate("click");
+
+    expect(store.getActions()).toEqual([
+      { pokemon: { name: "poke2", price: 100 }, type: "remove-pokemon" }
+    ]);
   });
 });
